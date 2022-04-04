0f0441455c8b6d285e4e16d7dad99e535b1bfd5e
gutierrez1077 <102771438+gutierrez1077@users.noreply.github.com>
2022-04-04 19:48:26 -0300

    Add files via upload

--- CentroOdonto_create.sql
+++ CentroOdonto_create.sql
@@ -90,3 +90,77 @@ CREATE TABLE Facturas (
 id_facturas 	INT NOT NULL PRIMARY KEY
 );
 
+-- Creacion de Claves Foraneas
+
+ALTER TABLE pacientes
+ADD Localidad INT;
+
+ALTER TABLE pacientes
+ADD CONSTRAINT fk_localidad FOREIGN KEY (Localidad) REFERENCES Localidades(id_localidad);
+
+ALTER TABLE pacientes
+ADD ObraSocial INT;
+
+ALTER TABLE pacientes
+ADD CONSTRAINT fk_OOSS FOREIGN KEY (ObraSocial) REFERENCES OOSS(id_OOSS);
+
+ALTER TABLE insumos
+ADD Proveedor INT;
+
+ALTER TABLE insumos
+ADD CONSTRAINT fk_Proveedores FOREIGN KEY (Proveedor) REFERENCES proveedores(id_proveed);
+
+ALTER TABLE insumos
+ADD Cod_Servicio INT;
+
+ALTER TABLE insumos
+ADD CONSTRAINT fk_servicios FOREIGN KEY (Cod_Servicio) REFERENCES Servicios (Cod_Servicio);
+
+ALTER TABLE proveedores
+ADD Producto INT;
+
+ALTER TABLE proveedores
+ADD CONSTRAINT fk_producto FOREIGN KEY (Producto) REFERENCES insumos(id_insumos);
+
+ALTER TABLE profesionales
+ADD Localidad_PF INT;
+
+ALTER TABLE profesionales
+ADD CONSTRAINT fk_localidad_pf FOREIGN KEY (Localidad_PF) REFERENCES localidades(id_localidad);
+
+ALTER TABLE profesionales
+ADD Especialidad INT;
+
+ALTER TABLE profesionales
+ADD CONSTRAINT fk_especialidad FOREIGN KEY (Especialidad) REFERENCES especialidades(id_especialidad);
+
+ALTER TABLE facturas
+ADD Paciente INT;
+
+ALTER TABLE facturas
+ADD CONSTRAINT fk_paciente FOREIGN KEY (Paciente) REFERENCES pacientes(id_paciente);
+
+ALTER TABLE facturas
+ADD Obra_Soc INT;
+
+ALTER TABLE facturas
+ADD CONSTRAINT OOSS_px FOREIGN KEY (Obra_Soc) REFERENCES ooss(id_OOSS);
+
+ALTER TABLE facturas
+ADD Legajo_PF INT;
+
+ALTER TABLE facturas
+ADD CONSTRAINT fk_legajo_pf FOREIGN KEY (Legajo_PF) REFERENCES profesionales(Legajo);
+
+ALTER TABLE facturas
+ADD Sucursal INT;
+
+ALTER TABLE facturas
+ADD CONSTRAINT fk_sucursal_factura FOREIGN KEY (Sucursal) REFERENCES sucursales(id_sucursal);
+
+ALTER TABLE facturas
+ADD Servicio_factura INT;
+
+ALTER TABLE facturas
+ADD CONSTRAINT fk_servicio_factura FOREIGN KEY (Servicio_factura) REFERENCES servicios(Cod_Servicio);
+
